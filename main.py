d620fa7edd3f1d4c71a0e095eabdec7ac45af3fb
ryu1707 <119115914+UIT-21520703@users.noreply.github.com>
2024-06-10 20:44:09 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import time
-import webbrowser as wb
 st.title('Đăng nhập')
 username = st.text_input('Username', '')
 password = st.text_input('Password', '', type='password')
@@ -8,7 +7,7 @@ if st.button('Đăng nhập'):
     if username == '21520703' and password == '2003':
         st.success('Đăng nhập thành công')
         time.sleep(2)
-        wb.open('http://www.uit.edu.vn/')
+        open('http://www.uit.edu.vn/')
     else:
         st.error('Sai username hoặc password')
 
